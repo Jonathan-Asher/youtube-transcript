dd4f37b83e893aa4354aa0a72c694531218322ee
Assaf-Megirov <assafmegirov@gmail.com>
2024-08-30 11:18:56 +0300

    Added final index file without unecessary changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -14,7 +14,10 @@ export declare class YoutubeTranscriptNotAvailableError extends YoutubeTranscrip
     constructor(videoId: string);
 }
 export declare class YoutubeTranscriptNotAvailableLanguageError extends YoutubeTranscriptError {
-    constructor(langs: string | string[], availableLangs: string[], videoId: string);
+    constructor(langs: string[], availableLangs: string[], videoId: string);
+}
+export declare class YoutubeVideoMetadataNotFoundError extends YoutubeTranscriptError {
+    constructor(videoPage?: string, message?: string);
 }
 export interface TranscriptConfig {
     lang?: string;
@@ -26,19 +29,62 @@ export interface TranscriptResponse {
     offset: number;
     lang?: string;
 }
+export interface IYoutubeVideoMetadata {
+    creator?: string;
+    creatorUsername?: string;
+    title?: string;
+    description?: string;
+    length?: number;
+    uploadDate?: Date;
+    postUrl?: string;
+    postId?: string;
+    videoUrl?: string;
+    fullUrl?: string;
+    thumbnailUrl?: string;
+    isAd?: boolean;
+    crosspost?: boolean;
+}
 /**
  * Class to retrieve transcript if exist
  */
 export declare class YoutubeTranscript {
     /**
-     * Fetch transcript from YTB Video
+     * Fetch transcript from YTB Video (legacy overload)
      * @param videoId Video url or video identifier
      * @param config Get transcript in a specific language ISOs, ordered by preference
+     * @returns Promise<TranscriptResponse[]>
      */
-    static fetchTranscript(videoId: string, config?: TranscriptConfig): Promise<TranscriptResponse[]>;
+    static fetchTranscript(//legacy overload
+    videoId: string, config?: TranscriptConfig): Promise<TranscriptResponse[]>;
+    /**
+     * fetch transcript from YTB Video (new overload: can include metadata)
+     * @param videoId
+     * @param config
+     * @param includeMetadata
+     * @returns {transcriptResponseArray: TranscriptResponse[], videoMetadata: IYoutubeVideoMetadata}
+     */
+    static fetchTranscript(//legacy overload
+    videoId: string, config?: TranscriptConfig, includeMetadata?: boolean): Promise<{
+        transcriptResponseArray: TranscriptResponse[];
+        videoMetadata: IYoutubeVideoMetadata;
+    }>;
     /**
      * Retrieve video id from url or string
      * @param videoId video url or video id
      */
     private static retrieveVideoId;
+    /**
+   * Fetches metadata for a YouTube video. Use this function if you want only the video's metadata without the transcript.
+   *
+   * @param {string} videoId - The ID of the YouTube video.
+   * @return {Promise<IYoutubeVideoMetadata>} A promise that resolves with the video's metadata. hint: import { IYoutubeVideoMetadata } from 'youtube-transcript';
+   */
+    static fetchMetadata(videoId: string): Promise<IYoutubeVideoMetadata>;
+    /**
+     * Extracts metadata from a YouTube video page as text.
+     *
+     * @param {string} videoPageBody - The response from the youtube video page as text. ex: response = await fetch(); videoPageBody = await response.text();
+     * @return {IYoutubeVideoMetadata} The extracted metadata.
+     */
+    private static getVideoMetaData;
 }
--- dist/youtube-transcript.esm.js
+++ dist/youtube-transcript.esm.js
@@ -53,20 +53,21 @@ class YoutubeTranscriptNotAvailableError extends YoutubeTranscriptError {
 }
 class YoutubeTranscriptNotAvailableLanguageError extends YoutubeTranscriptError {
     constructor(langs, availableLangs, videoId) {
-        const langsStr = Array.isArray(langs) ? langs.join(', ') : langs;
-        super(`No transcripts are available in ${langsStr} for this video (${videoId}). Available languages: ${availableLangs.join(', ')}`);
+        super(`No transcripts are available in ${langs.join(', ')} for this video (${videoId}). Available languages: ${availableLangs.join(', ')}`);
+    }
+}
+class YoutubeVideoMetadataNotFoundError extends YoutubeTranscriptError {
+    constructor(videoPage, message) {
+        if (videoPage.length > 8000)
+            videoPage = videoPage.slice(0, 4000) + '...<object too long>' + videoPage.slice(-4000);
+        super(`Video metadata not found. ${message}` + (videoPage ? ` (Video page: ${videoPage})` : ``));
     }
 }
 /**
  * Class to retrieve transcript if exist
  */
 class YoutubeTranscript {
-    /**
-     * Fetch transcript from YTB Video
-     * @param videoId Video url or video identifier
-     * @param config Get transcript in a specific language ISOs, ordered by preference
-     */
-    static fetchTranscript(videoId, config) {
+    static fetchTranscript(videoId, config, includeMetadata) {
         var _a, _b;
         return __awaiter(this, void 0, void 0, function* () {
             const identifier = this.retrieveVideoId(videoId);
@@ -103,7 +104,6 @@ class YoutubeTranscript {
             }
             // Check for available languages based on config
             let availableLanguages = configLangs.filter((lang) => captions.captionTracks.some((track) => track.languageCode === lang));
-            console.log(captions.captionTracks.map((track) => track.languageCode));
             if (configLangs.length && !availableLanguages.length) {
                 throw new YoutubeTranscriptNotAvailableLanguageError(configLangs, captions.captionTracks.map((track) => track.languageCode), videoId);
             }
@@ -112,19 +112,24 @@ class YoutubeTranscript {
                 ? captions.captionTracks.find((track) => track.languageCode === transcriptLanguage)
                 : captions.captionTracks[0]).baseUrl;
             const transcriptResponse = yield fetch(transcriptURL, {
-                headers: Object.assign(Object.assign({}, (((config === null || config === void 0 ? void 0 : config.langs) || (config === null || config === void 0 ? void 0 : config.lang)) && { 'Accept-Language': transcriptLanguage })), { 'User-Agent': USER_AGENT }),
+                headers: Object.assign(Object.assign({}, (transcriptLanguage && { 'Accept-Language': transcriptLanguage })), { 'User-Agent': USER_AGENT }),
             });
             if (!transcriptResponse.ok) {
                 throw new YoutubeTranscriptNotAvailableError(videoId);
             }
             const transcriptBody = yield transcriptResponse.text();
             const results = [...transcriptBody.matchAll(RE_XML_TRANSCRIPT)];
-            return results.map((result) => ({
+            const finalResults = results.map((result) => ({
                 text: result[3],
                 duration: parseFloat(result[2]),
                 offset: parseFloat(result[1]),
                 lang: transcriptLanguage,
             }));
+            if (includeMetadata) {
+                const metaData = YoutubeTranscript.getVideoMetaData(videoPageBody);
+                return { transcriptResponseArray: finalResults, videoMetadata: metaData };
+            }
+            return finalResults;
         });
     }
     /**
@@ -141,6 +146,81 @@ class YoutubeTranscript {
         }
         throw new YoutubeTranscriptError('Impossible to retrieve Youtube video ID.');
     }
+    /**
+   * Fetches metadata for a YouTube video. Use this function if you want only the video's metadata without the transcript.
+   *
+   * @param {string} videoId - The ID of the YouTube video.
+   * @return {Promise<IYoutubeVideoMetadata>} A promise that resolves with the video's metadata. hint: import { IYoutubeVideoMetadata } from 'youtube-transcript';
+   */
+    static fetchMetadata(videoId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const identifier = this.retrieveVideoId(videoId);
+            const videoPageResponse = yield fetch(`https://www.youtube.com/watch?v=${identifier}`, {
+                headers: {
+                    'User-Agent': USER_AGENT
+                },
+            });
+            const videoPageBody = yield videoPageResponse.text();
+            if (videoPageBody.includes('class="g-recaptcha"')) {
+                throw new YoutubeTranscriptTooManyRequestError();
+            }
+            if (!videoPageBody.includes('"playabilityStatus":')) {
+                throw new YoutubeTranscriptVideoUnavailableError(videoId);
+            }
+            return YoutubeTranscript.getVideoMetaData(videoPageBody);
+        });
+    }
+    /**
+     * Extracts metadata from a YouTube video page as text.
+     *
+     * @param {string} videoPageBody - The response from the youtube video page as text. ex: response = await fetch(); videoPageBody = await response.text();
+     * @return {IYoutubeVideoMetadata} The extracted metadata.
+     */
+    static getVideoMetaData(videoPageBody) {
+        let startSplit, jsonString, jsonObject;
+        try {
+            startSplit = videoPageBody.split('var ytInitialPlayerResponse = ')[1];
+            jsonString = startSplit.split(';</script>')[0];
+        }
+        catch (e) {
+            let lastObj = jsonString ? jsonString : startSplit;
+            if (!lastObj)
+                lastObj = videoPageBody;
+            throw new YoutubeVideoMetadataNotFoundError(lastObj, 'Couldnt split html by: "var ytInitialPlayerResponse = " or ";</script>"' + e.message);
+        }
+        //trim all the characters from the end of jsonString that are not '}'
+        while (jsonString && jsonString[jsonString.length - 1] !== '}') {
+            jsonString = jsonString.slice(0, -1);
+        }
+        try {
+            jsonObject = JSON.parse(jsonString);
+        }
+        catch (e) {
+            throw new YoutubeVideoMetadataNotFoundError(jsonString, 'Couldnt parse json, ' + e.message);
+        }
+        if (jsonObject.videoDetails) {
+            const videoDetails = jsonObject.videoDetails;
+            const res = {};
+            res.creator = videoDetails.author;
+            res.creatorUsername = videoDetails.channelId;
+            res.title = videoDetails.title;
+            res.description = videoDetails.shortDescription;
+            res.length = Number(videoDetails.lengthSeconds) * 1000; //length is storedin ms
+            if (jsonObject.microformat && jsonObject.microformat.playerMicroformatRenderer) {
+                const microformat = jsonObject.microformat.playerMicroformatRenderer;
+                res.uploadDate = new Date(microformat.uploadDate);
+            }
+            res.postUrl = `https://www.youtube.com/watch?v=${videoDetails.videoId}`;
+            res.postId = videoDetails.videoId;
+            res.videoUrl = undefined; //TODO:
+            res.fullUrl = undefined;
+            res.thumbnailUrl = videoDetails.thumbnail.thumbnails[0].url; //the smallest thumbnail
+            res.isAd = false; //TODO: figure out how to find out if it is an ad
+            res.crosspost = false; //NA in youtube
+            return res;
+        }
+        throw new YoutubeVideoMetadataNotFoundError(jsonString, 'parsed but didnt find the video details');
+    }
 }
 
-export { YoutubeTranscript, YoutubeTranscriptDisabledError, YoutubeTranscriptError, YoutubeTranscriptNotAvailableError, YoutubeTranscriptNotAvailableLanguageError, YoutubeTranscriptTooManyRequestError, YoutubeTranscriptVideoUnavailableError };
+export { YoutubeTranscript, YoutubeTranscriptDisabledError, YoutubeTranscriptError, YoutubeTranscriptNotAvailableError, YoutubeTranscriptNotAvailableLanguageError, YoutubeTranscriptTooManyRequestError, YoutubeTranscriptVideoUnavailableError, YoutubeVideoMetadataNotFoundError };
